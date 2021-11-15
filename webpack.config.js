bc860e825b8a880c3d721afbf08a9dc98b9c4ffa
Ivan Che <ivan.chepurin@gmail.com>
2021-11-15 14:25:38 +0300

    dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require('path')
 
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanTerminalPlugin = require('clean-terminal-webpack-plugin')
 
 module.exports = env => ({
    mode: env.production ? 'production' : 'development',
    entry: './src/entry.js',
-   plugins: [new HtmlWebpackPlugin()],
+   plugins: [new HtmlWebpackPlugin(), new CleanTerminalPlugin()],
    output: {
       path: path.resolve(__dirname, './dist'),
       clean: true
@@ -14,5 +15,8 @@ module.exports = env => ({
    devtool: 'inline-source-map',
    devServer: {
       static: './dist'
+   },
+   stats: {
+      preset: 'errors-warnings'
    }
 })
